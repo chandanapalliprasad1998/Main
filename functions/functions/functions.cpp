e43eb062c5f7d551ffc51f98cfafce35b426be87
chandanapalli prasad <chandanapalliprasad1998@gmail.com>
2024-12-04 18:39:48 +0530

    I am adding functions

---
+++ functions/functions/functions.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void fun()
+{
+	printf("fun function\n");
+}
+int main()
+{
+	printf("hello\n");
+	printf("hello1\n");
+	fun();
+	printf("hello2\n");
+	printf("hello3\n");
+	printf("hello4\n");
+	fun();
+}
